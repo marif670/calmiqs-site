42646f58e625034e8e5a33daefdfacc6395fd9fd
marif670 <arif670@gmail.com>
2025-11-03 12:39:32 +0500

    Delete update-post.js

--- functions/update-post.js
+++
@@ -1,5 +0,0 @@
-export async function onRequest(context) {
-  return new Response(JSON.stringify({ success: true, message: "Function reached" }), {
-    headers: { "Content-Type": "application/json" },
-  });
-}
