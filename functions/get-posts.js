342df32e7a74e5cf2e7ecb5da97f18c4cad3614b
marif670 <arif670@gmail.com>
2025-11-03 21:34:33 +0500

    automatic workflow

--- functions/get-posts.js
+++ functions/get-posts.js
@@ -1,13 +1,20 @@
+// /functions/get-posts.js
 export async function onRequestGet({ env }) {
-  try {
-    const posts = (await env.CALMIQS_POSTS.get("posts", "json")) || [];
-    return new Response(JSON.stringify(posts), {
-      headers: { "Content-Type": "application/json" },
-    });
-  } catch (err) {
-    return new Response(JSON.stringify({ error: err.message }), {
-      status: 500,
-      headers: { "Content-Type": "application/json" },
-    });
+  const list = await env.CALMIQS_POSTS.list();
+  const results = [];
+
+  for (const key of list.keys) {
+    const postData = await env.CALMIQS_POSTS.get(key.name);
+    if (postData) {
+      try {
+        results.push({ id: key.name, ...JSON.parse(postData) });
+      } catch {
+        console.error("Invalid JSON for key:", key.name);
+      }
+    }
   }
+
+  return new Response(JSON.stringify(results, null, 2), {
+    headers: { "Content-Type": "application/json" },
+  });
 }
